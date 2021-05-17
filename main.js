d3937b68d86695420396ba1e0c35b5f56ce1fb09
Scott Alton <scottaltonmusic@gmail.com>
2021-05-17 11:07:52 -0400

    Added function to process data recieved and create an object to return for rendering.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const navbar = document.querySelector('nav');
 
 console.log("Let's try a request targetting " + img + ': ');
 
-async function getWeather(city) {
+async function getCurrentWeather(city) {
   try {
     const res = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&cnt=7&APPID=e89da154536fd30ccf31b6e56206ca6e`
@@ -30,13 +30,62 @@ async function getWeather(city) {
       console.log(data.message);
       // return;
     }
+
+    let weatherData = processWeatherData(data);
+    console.log('HERES AN OVERVIEW OF DATA RETRIEVED AS AN OBJECT:');
+    console.table(weatherData);
   } catch (err) {
     console.log('No weather data found for the provided city.');
   }
 }
-getWeather('Whitby');
+getCurrentWeather('Whitby');
 
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
-  getWeather(searchBar.value);
+  getCurrentWeather(searchBar.value);
 });
+
+function processWeatherData(data) {
+  console.log('THIS IS DA DATA MAN: ');
+  console.table(data);
+
+  let city = data.name;
+  let date = new Date().toLocaleString().split(',')[0];
+  let weather = data.weather[0].main;
+  let temp = data.main.temp;
+  let feelsLikeTemp = data.main.feels_like;
+  let humidity = data.main.humidity;
+  let wind = data.wind.speed;
+
+  let weatherData = {
+    city,
+    date,
+    weather,
+    temp,
+    feelsLikeTemp,
+    humidity,
+    wind,
+  };
+
+  return weatherData;
+}
+
+async function getForcast(city) {
+  try {
+    const res = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=7&APPID=e89da154536fd30ccf31b6e56206ca6e`
+    );
+    const data = await res.json();
+
+    console.log(data);
+
+    if (data.cod === '404' || data.code === '400') {
+      console.log(data.message);
+      // return;
+    }
+
+    processData(data);
+  } catch (err) {
+    console.log('No weather data found for the provided city.');
+  }
+}
