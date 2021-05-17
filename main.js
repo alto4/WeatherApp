429479b98e0dd43cb0818e5698bfe5650e1c0a51
Scott Alton <scottaltonmusic@gmail.com>
2021-05-17 14:46:47 -0400

    Final styling touches and refactoring made.

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // Sample API request #1 : http://api.openweathermap.org/data/2.5/weather?q=Toronto,ca&APPID=e89da154536fd30ccf31b6e56206ca6e
-// Sample API request #2 : https://api.giphy.com/v1/gifs/translate?api_key=SoVlRBmtCajA9B1z7own0z3l0wLKZjB9&s=cats
+
 const img = document.querySelector('img');
 const searchBar = document.querySelector('[name=search]');
 const weatherContainer = document.querySelector('.weather-container');
 const submitButton = document.querySelector('.btn-submit');
 const navbar = document.querySelector('nav');
 
-console.log("Let's try a request targetting " + img + ': ');
-
+// Get city data
 async function getCurrentWeather(city) {
   try {
     const res = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&cnt=7&APPID=e89da154536fd30ccf31b6e56206ca6e&units=metric`
     );
+
     const data = await res.json();
 
-    console.log(data);
+    // If request is successful, display city name in DOM
     if (data.cod === 200) {
       let cityHeading = document.querySelector('.city-name');
       cityHeading.innerText = '';
@@ -26,30 +26,31 @@ async function getCurrentWeather(city) {
       }
     }
 
+    // If any errors arise, display them in the console
     if (data.cod === '404' || data.code === '400') {
-      console.log(data.message);
-      // return;
+      console.error(data.message);
     }
 
+    // Process, then render data in DOM
     let weatherData = processWeatherData(data);
-    console.log('HERES AN OVERVIEW OF DATA RETRIEVED AS AN OBJECT:');
-    // console.table(weatherData);
     renderData(weatherData);
   } catch (err) {
-    console.log('No weather data found for the provided city.');
+    console.error('No weather data found for the provided city.');
   }
 }
-getCurrentWeather('Whitby');
 
+// Default to display Toronto's weather upon initial load
+getCurrentWeather('Toronto');
+
+// When submit button is clicked, make API request and clear searchbar
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
   getCurrentWeather(searchBar.value);
+  searchBar.value = '';
 });
 
+// Process data from API response and return a neater object representation containing only needed data
 function processWeatherData(data) {
-  console.log('THIS IS DA DATA MAN: ');
-  //console.table(data);
-
   let city = data.name;
   let date = new Date().toLocaleString().split(',')[0];
   let weather = data.weather[0].main;
@@ -73,72 +74,54 @@ function processWeatherData(data) {
   return weatherData;
 }
 
-function processForecastData(data) {
-  console.clear();
-  console.log('PROCESSING FORECAST DATA HERE');
-  console.table(data.list);
-}
-async function getForcast(city) {
-  try {
-    const res = await fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=7&APPID=e89da154536fd30ccf31b6e56206ca6e&unit=metric&cnt=24`
-    );
-    const data = await res.json();
-
-    console.log(data);
-
-    if (data.cod === '404' || data.code === '400') {
-      console.log(data.message);
-      // return;
-    }
-
-    processForecastData(data);
-  } catch (err) {
-    console.log('No weather data found for the provided city.');
-  }
-}
-
+// Render weather data in DOM
 function renderData(data) {
   let { city, icon, date, weather, temp, feelsLikeTemp, humidity, wind } = data;
 
   weatherContainer.innerHTML = `
+  <h1 class="temperature">${Math.round(temp)}<span>&deg;C</span></h1>
   <h1>${city}</h1>
   <img src="http://openweathermap.org/img/w/${icon}.png" alt="${weather} icon." />
-  <h2>${weather}</h2>
-  <h2>Temperature: ${temp}&deg;C</h2>
+  <h2 class="weather-description">${weather}</h2>
   <h2>Feels Like: ${feelsLikeTemp}&deg;C</h2>
   <h2>Humidity: ${humidity}</h2>
   <h2>Wind: ${(wind * 3.6).toFixed(2)} KM/H</h2>
   `;
+
+  // Update background image according to currently displayed city's weather
+  evaluateBackgroundImage(weather);
 }
 
+// Based on current city's weather, choose an appropriate background image
 function evaluateBackgroundImage(weather) {
   let body = document.querySelector('body');
 
   switch (weather.toLowerCase()) {
     case 'clouds':
-      console.log('CLOUD IMAGE HERE');
       body.style.background = `linear-gradient(
         rgba(0, 0, 0, .78), 
         rgba(0, 0, 0, .99)
       ), url('./images/cloudy.jpg')`;
       break;
     case 'clear':
-      console.log('CLEAR IMAGE HERE');
       body.style.background = `linear-gradient(
         rgba(0, 0, 0, .78), 
         rgba(0, 0, 0, .99)
       ), url('./images/clear.jpg')`;
       break;
     case 'snow':
-      console.log('SNOW IMAGE HERE');
       body.style.background = `linear-gradient(
         rgba(0, 0, 0, .78), 
         rgba(0, 0, 0, .99)
       ), url('./images/snow.jpg')`;
       break;
+    case 'rain':
+      body.style.background = `linear-gradient(
+        rgba(0, 0, 0, .5), 
+        rgba(0, 0, 0, .4)
+      ), url('./images/rain.jpg')`;
+      break;
     default:
-      console.log('default image here');
       body.style.background = `linear-gradient(
         rgba(0, 0, 0, .78), 
         rgba(0, 0, 0, .99)
