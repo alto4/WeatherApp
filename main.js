4b9b058a6acd3f2e1a5cdd616e4d97d067498e5d
Scott Alton <scottaltonmusic@gmail.com>
2021-05-16 21:27:10 -0400

    Added calls for weather data and updating city name upon successful response.

--- main.js
+++ main.js
@@ -2,19 +2,36 @@
 // Sample API request #2 : https://api.giphy.com/v1/gifs/translate?api_key=SoVlRBmtCajA9B1z7own0z3l0wLKZjB9&s=cats
 const img = document.querySelector('img');
 const searchBar = document.querySelector('[name=search]');
+const weatherContainer = document.querySelector('.weather-container');
 
 console.log("Let's try a request targetting " + img + ': ');
 
-async function getImage(searchTerm) {
+async function getWeather(city) {
   const res = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=SoVlRBmtCajA9B1z7own0z3l0wLKZjB9&s=${searchTerm}`
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=e89da154536fd30ccf31b6e56206ca6e`
   );
-  const gifData = await res.json();
-  img.src = gifData.data.images.original.url;
+  const data = await res.json();
+
+  if (data) {
+    // img.src = gifData.data.images.original.url;
+    console.log("Here's your data: " + data.weather);
+    console.table(data);
+    console.table(data.weather);
+    console.log(data.name);
+
+    let cityHeading = document.createElement('h1');
+
+    if (data.name) {
+      weatherContainer.innerHTML = '';
+      cityHeading.innerText = data.name + ', ' + data.sys.country;
+      weatherContainer.appendChild(cityHeading);
+    } else {
+      weatherContainer.innerHTML = '';
+    }
+  }
 }
+getWeather('Whitby');
 
-getImage('banana');
-
-searchBar.addEventListener('keypress', () => {
-  getImage(searchBar.value);
+searchBar.addEventListener('keyup', () => {
+  getWeather(searchBar.value);
 });
